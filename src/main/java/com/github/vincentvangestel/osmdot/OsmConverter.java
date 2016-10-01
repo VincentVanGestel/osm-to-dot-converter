7033ff3e802e635a7b7651bff7f1660be0b1418f
Vincent Van Gestel <vincent.vgestel@gmail.com>
2016-10-01 13:17:10 +0200

    Removed more (now) unused stuff

--- src/main/java/com/github/vincentvangestel/osmdot/OsmConverter.java
+++ src/main/java/com/github/vincentvangestel/osmdot/OsmConverter.java
@@ -23,8 +23,7 @@ import com.google.common.base.Optional;
 
 /**
  * This class is responsible for guiding the conversion process.
- * It will call the {@link OsmReader} to load the data,
- * it will convert the loaded data to the new format,
+ * It will convert the loaded data to the new format,
  * it will call the {@link DotWriter} to write the dot file.
  */
 public class OsmConverter {
